28b010b74dd939f58f324d0ec1782c94c66573a0
Charlotte Lopp <clopp@dons.usfca.edu>
2017-09-09 10:23:38 -0700

    Commiting SetupProject.sh by itself

---
+++ SetupProject.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Charlotte Lopp
+# clopp@dons.usfca.edu
+# September 9, 2017
+
+# Making directory and file structure
+cd CharlotteLopp-week03-homework-git
+mkdir code data output data/raw_data output/figures output/tables
+echo "Made project infrastructure" 
+
+# Download fasta files from the internet and unzip them
+cd data/raw_data
+curl -L http://npk.io/PUlBaY+ -o fasta-archive.zip
+unzip fasta-archive.zip
+mv *.fasta ../data
+cd data/raw_data
+rm fasta-archive.zip
+mv *.fasta data/raw_data
+echo "Downloaded fasta files and deleted unzipped archive"
+
